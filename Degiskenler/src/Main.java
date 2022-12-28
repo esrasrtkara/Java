a0f2d993338786a3e2825ff7e1cfa8facdb557cf
Esra AKBULUT <56135111+esrasrtkara@users.noreply.github.com>
2022-12-28 17:44:01 +0300

    added degiskenler

---
+++ Degiskenler/src/Main.java
@@ -0,0 +1,28 @@
+/*
+        int veri tipi 32 bitlik yer kaplıyor. -2^^-31 -  2^^31
+        byte 8 bitlik yer kaplar.
+        short 16 bitlik yer kaplar.
+        */
+public class Main {
+    
+    public static void main(String[] args) {
+     
+     //byte --> short --> int --> long  
+       
+    byte a = 100;
+    
+    byte b = (byte)(a/2);
+        
+    
+    short i = 100;
+    
+    byte j = 2;
+    
+    int k = 4;
+    
+    
+    long d = i+j+k;
+        
+        
+    }
+}
