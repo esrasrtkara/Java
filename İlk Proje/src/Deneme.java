56fef1399dbd51ec6073cd0c98e1e8618c6331b3
Esra AKBULUT <56135111+esrasrtkara@users.noreply.github.com>
2022-12-28 13:06:27 +0300

    added İlk Proje

---
+++ İlk Proje/src/Deneme.java
@@ -0,0 +1,13 @@
+
+public class Deneme {
+    
+    public static void main(String[] args){
+        
+        System.out.println("Merhaba Java");
+        
+        System.out.println("Hoşgeldiniz...");
+    }
+    
+    
+    
+}
