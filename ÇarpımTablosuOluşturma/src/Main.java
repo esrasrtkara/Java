56c5a4a755be80d28c9266caf4cbd2f015149105
Esra AKBULUT <56135111+esrasrtkara@users.noreply.github.com>
2023-01-05 13:55:21 +0300

    add

---
+++ ÇarpımTablosuOluşturma/src/Main.java
@@ -0,0 +1,20 @@
+/*
+    İç içe for döngüsü kullanarak çarpım tablosu oluşturun.
+*/
+public class Main {
+    
+    public static void main(String[] args) {
+  
+        for(int i=1; i<=10;i++)
+        {
+            System.out.println("*****************************");
+            for(int j= 1;j<=10;j++)
+            {
+                System.out.println(i+"x"+j+"=" +j*i);
+            }
+            System.out.println("*****************************");
+        }
+        
+    }
+    
+}
