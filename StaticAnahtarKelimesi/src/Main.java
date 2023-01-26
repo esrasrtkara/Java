384c88805d31cdb7b891a59371782f500859d89e
Esra AKBULUT <56135111+esrasrtkara@users.noreply.github.com>
2023-01-26 18:07:23 +0300

    add

---
+++ StaticAnahtarKelimesi/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        //Seyirci seyirci1 = new Seyirci("Esra Sırtıkara");
+      //  Seyirci seyirci2 = new Seyirci("Halil Akbulut");
+        
+       // seyirci1.oyun_seyret();
+        
+        System.out.println("Seyirci Sayısı : "+ Seyirci.seyirci_sayisi);
+        
+    }
+    
+}
