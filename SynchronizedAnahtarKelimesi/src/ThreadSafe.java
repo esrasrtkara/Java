82a2dbcbe7fb89cd25f92f5ccd9532f0b44ddefe
Esra AKBULUT <56135111+esrasrtkara@users.noreply.github.com>
2023-02-10 20:10:45 +0300

    add

---
+++ SynchronizedAnahtarKelimesi/src/ThreadSafe.java
@@ -0,0 +1,62 @@
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class ThreadSafe {
+    
+    private  int count = 0;
+    
+    public synchronized void artir(){
+        
+        count++;
+    }
+    
+    public void threadleriCalistir(){
+        
+        Thread thread1;
+        thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+               for(int i = 0 ; i < 5000 ; i++){
+                   artir();
+               }
+            }
+        });
+        Thread thread2;
+        thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+               for(int i = 0 ; i < 5000 ; i++){
+                   artir();
+               }
+            }
+        });
+        
+        thread1.start();
+        thread2.start();
+        
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(ThreadSafe.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        System.out.println("Count değişkenin değeri : "+ count);
+       
+    }
+    
+    public static void main(String[] args) {
+        
+        ThreadSafe threadSafe = new ThreadSafe();
+        
+        
+        threadSafe.threadleriCalistir();
+        
+        
+        
+    }
+    
+}
