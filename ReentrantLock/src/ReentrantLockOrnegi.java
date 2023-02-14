3bbddb7f90a3110772ef82efa50edfdbe6a6cd5d
Esra AKBULUT <56135111+esrasrtkara@users.noreply.github.com>
2023-02-14 17:07:42 +0300

    add

---
+++ ReentrantLock/src/ReentrantLockOrnegi.java
@@ -0,0 +1,64 @@
+
+import java.util.Scanner;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class ReentrantLockOrnegi {
+    
+    private int say = 0;
+    private Lock lock = new ReentrantLock();
+    private Condition condition = lock.newCondition();
+    
+    public void artir(){
+         for(int i = 0; i < 10000;i++){
+          say++;
+         }
+    }
+    
+    public  void  thread1Fonksiyonu(){
+       lock.lock();
+        System.out.println("Thread 1 çalışıyor...");
+       System.out.println("Thread 1 Uyandırılmayı bekliyor...");
+        
+        try {
+            condition.await();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(ReentrantLockOrnegi.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println("Thread 1 uyandırıldı ve işlemine devam ediyor...");
+        artir();
+     
+       lock.unlock();
+       
+    }
+    public void thread2Fonksiyonu(){
+        
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(ReentrantLockOrnegi.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Scanner scanner = new Scanner(System.in);
+     
+        lock.lock();
+        System.out.println("Thread 2 çalışıyor....");
+   
+        artir();
+        System.out.println("Devam etmek için bir tuşa basınız...");
+        scanner.nextLine();
+        
+        condition.signal();
+        System.out.println("Thread 1 uyandırıldı. Ben gidiyorum.");
+        lock.unlock();
+        
+    }
+    
+    public void threadOver(){
+        
+        System.out.println("Say değişkeninin değeri : " + say);
+    }
+}
