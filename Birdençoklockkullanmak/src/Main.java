454d717f1914533fe4bedcff736eb81d77c9a2de
Esra AKBULUT <56135111+esrasrtkara@users.noreply.github.com>
2023-02-14 11:52:56 +0300

    add

---
+++ Birdençoklockkullanmak/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        ListWorker worker = new ListWorker();
+      /*  long baslangic = System.currentTimeMillis();
+        
+        worker.degerAta();
+        
+        long bitis = System.currentTimeMillis();
+        
+        System.out.println("Geçen süre : " +(bitis-baslangic));*/
+      
+      worker.calistir();
+    
+        
+        
+        
+    }
+    
+}
