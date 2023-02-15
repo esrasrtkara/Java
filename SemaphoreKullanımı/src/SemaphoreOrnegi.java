991d31a28a15ba9281c8f605379d938b05d2183a
Esra AKBULUT <56135111+esrasrtkara@users.noreply.github.com>
2023-02-15 12:22:29 +0300

    add

---
+++ SemaphoreKullanımı/src/SemaphoreOrnegi.java
@@ -0,0 +1,34 @@
+
+import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class SemaphoreOrnegi {
+    private Semaphore sem = new Semaphore(3);
+    
+    
+    public void threadFonksiyonu(int id){
+        
+        try {
+            sem.acquire();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(SemaphoreOrnegi.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        System.out.println("Theread başlıyor... ID : " + id);
+        
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(SemaphoreOrnegi.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println("Thread Çıkıyor... ID : "+ id);
+        
+        
+        sem.release();
+        
+        
+    }
+    
+}
